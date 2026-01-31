e10d8494557e814e187c67381609fd2703137e58
MMH Group <mafiammh@gmail.com>
2026-01-31 05:37:05 +0000

    .

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Product from './Product.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>Product Display</h1>
+      <Product name="Laptop" price={75000} />
+    </div>
+  )
+}
+
+export default App
